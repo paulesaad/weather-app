fe42b0871dfb735b3a4b68ce1c83771d008b31fa
paulesaad <paulsaad.13@gmail.com>
2015-06-04 17:26:31 -0500

    functionalization of lower bar using eventListeners and loading of google maps api, not implemented yet in code

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -9,6 +9,7 @@ var key = 'b368ef0dc47a771e46425a036ab6def1'
 var GPS = new Promise((res, rej) => {
     // if gps successful, resolve with coordinates
     // else reject with error
+    
     navigator.geolocation.getCurrentPosition(
         (gpsData) => res({
             lat: gpsData.coords.latitude,
@@ -23,6 +24,17 @@ GPS.then((ll) => {
         return document.querySelector(selector)
     }
 
+    function qsAll(selector) {
+        return document.querySelectorAll(selector)
+    }
+
+    var address = `Houston, TX`
+    var googurl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`
+    var y = $.getJSON(googurl).then((r) => {
+        console.log(r)
+    })
+
+
     var url = `https://api.forecast.io/forecast/${key}/${ll.lat},${ll.lon}?callback=?`
 
     var x = $.getJSON(url).then((r) => {
@@ -32,47 +44,54 @@ GPS.then((ll) => {
         qs("#ticker").innerHTML = weatheralert
 
 
-        //Quick weather data
-        var current_data = r.currently //object
-        var temp_current = current_data.temperature
-        var appTemp_current = current_data.apparentTemperature
-        var precProb_current = `${current_data.precipProbability*100}%`
-        var summary_current = current_data.summary
-        var visibility = current_data.visibility
-        var humidity_current = current_data.humidity
+        //Quick weather data (weather currently)
+        var current_data = r.currently,
+            temp_current = current_data.temperature,
+            appTemp_current = current_data.apparentTemperature,
+            precProb_current = current_data.precipProbability * 100,
+            summary_current = current_data.summary,
+            visibility = current_data.visibility,
+            humidity_current = current_data.humidity * 100,
+            wind_current = current_data.windSpeed
 
-        qs(".current").innerHTML += temp_current
-        qs(".current").innerHTML += appTemp_current
-        qs(".current").innerHTML += precProb_current
-        qs(".current").innerHTML += summary_current
-        qs(".current").innerHTML += visibility
-        qs(".current").innerHTML += humidity_current
+        qs(".current").innerHTML += `<span>Summary for the day --> ${summary_current}</span>`
+        qs(".current").innerHTML += `<span>Now --> ${Math.round(temp_current)}\xB0</span>`
+        qs(".current").innerHTML += `<span>Feels Like --> ${Math.round(appTemp_current)}\xB0</span>`
+        qs(".current").innerHTML += `<span>Chance of rain --> ${Math.round(precProb_current)}%</span>`
+        qs(".current").innerHTML += `<span>Wind --> ${wind_current} mph</span>`
+        qs(".current").innerHTML += `<span>Visibility --> ${visibility} mi</span>`
+        qs(".current").innerHTML += `<span>Humidity --> ${humidity_current}%</span>`
 
 
-        //Daily forecast data
-        var daily_data = r.daily.data[0] //array
-        var highLow = `${Math.round(daily_data.temperatureMax)}\xB0/${Math.round(daily_data.temperatureMin)}\xB0`
-        var feelsLike = `Feels like: ${Math.round(daily_data.apparentTemperatureMax)}\xB0/${Math.round(daily_data.apparentTemperatureMin)}\xB0`
-        var chance_of_precipitation = `Chance of precipitation: ${daily_data.precipProbability*100}%`
-        var summary = `${daily_data.summary}`
-        var weather_icon_url = function(dailydata) {
-            if (dailydata.cloudCover < .3) {
+        var weather_icon_src_generator = function(dataOnDay) {
+            if (dataOnDay.cloudCover < .3) {
                 return `../../images/sunny.svg`
-            } else if (dailydata.cloudCover > .3 && dailydata.cloudCover < .5) {
+            } else if (dataOnDay.cloudCover > .3 && dataOnDay.cloudCover < .5) {
                 return `../../images/partlysunny.svg`
-            } else if (dailydata.cloudCover > 0.5 && dailydata.precipProbability < 0.5) {
+            } else if (dataOnDay.cloudCover > 0.5 && dataOnDay.precipProbability < 0.5) {
                 return `../../images/cloudy.svg`
             } else {
                 return `../../images/rainy.svg`
             }
         }
-        var weather_icon_tag = `<img src="${weather_icon_url(daily_data)}">`
 
-        qs(".daily").innerHTML = weather_icon_tag + `${qs(".daily").innerHTML}`
-        qs("#hiLo").innerHTML = highLow
-        qs("#apparentHiLo").innerHTML = feelsLike
-        qs("#chancePrecip").innerHTML = chance_of_precipitation
-        qs("#overview").innerHTML = summary
+        //Daily forecast data
+        function displayDayX(dayX) {
+            var daily_data = r.daily.data[dayX], //array
+                highLow = `${Math.round(daily_data.temperatureMax)}\xB0/${Math.round(daily_data.temperatureMin)}\xB0`,
+                feelsLike = `Feels like: ${Math.round(daily_data.apparentTemperatureMax)}\xB0/${Math.round(daily_data.apparentTemperatureMin)}\xB0`,
+                chance_of_precipitation = `Chance of precipitation: ${Math.round(daily_data.precipProbability*100)}%`,
+                summary = `${daily_data.summary}`,
+                weather_icon_src = weather_icon_src_generator(daily_data)
+
+            qs(".daily img").src = weather_icon_src
+            qs("#hiLo").innerHTML = highLow
+            qs("#apparentHiLo").innerHTML = feelsLike
+            qs("#chancePrecip").innerHTML = chance_of_precipitation
+            qs("#overview").innerHTML = summary
+        }
+
+        displayDayX(0)
 
         //Weekly Forecast data
         var week_summary = r.daily.summary
@@ -80,60 +99,31 @@ GPS.then((ll) => {
 
         var weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
         var days = [0, 1, 2, 3, 4, 5, 6, 7]
-        days.forEach((day) => {
-            var week_data = r.daily.data[day]
-            var week_highLow = `${Math.round(week_data.temperatureMax)}\xB0/${Math.round(week_data.temperatureMin)}\xB0`
-            var week_weather_icon_tag = `<img src="${weather_icon_url(week_data)}">`
-            // qs(".through_the_week div:nth-of-type(day+1) ").innerHTML = week_highLow
-            // qs().innerHTML = week_highLow
 
+        function getDay_of_week(day) {
+            return weekdays[((new Date().getDay() - 1) + day) % 7]
+        }
+
+
+        days.forEach((day) => {
+            var week_data = r.daily.data[day],
+                week_highLow = `${Math.round(week_data.temperatureMax)}\xB0/${Math.round(week_data.temperatureMin)}\xB0`,
+                week_weather_icon_src = `${weather_icon_src_generator(week_data)}`
+
+            qs(`.dayoftheweek:nth-of-type(${day+1}) span:first-child`).innerHTML = getDay_of_week(day)
+            qs(`.dayoftheweek:nth-of-type(${day+1}) img`).src = week_weather_icon_src
+            qs(`.dayoftheweek:nth-of-type(${day+1}) span:nth-of-type(2)`).innerHTML = week_highLow
         })
 
+        var daysOfWeekEls = qsAll('.dayoftheweek')
+        for (var i = 0; i < daysOfWeekEls.length; i++) {
+            daysOfWeekEls[i].addEventListener('click', function() {
+                displayDayX(parseInt(this.getAttribute('data-day')))
+                    // qs("h5").innerHTML = `${getDay_of_week(parseInt(this.getAttribute('data-day')))}'s Forecast`
+                    // console.log((parseInt(this.getAttribute('data-day'))))
+            })
+        }
 
     })
 
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Routing using hashchange
-
-// var GithubRouter = Backbone.Router.extend({
-//     routes: {
-//         'home': 'home'
-//         'weather/:lat/:lng' :
-//     },
-//     drawProfile: function(user){
-//         new GithubClient(user).createProfile()
-//     },
-//     initialize: function(){
-//         Backbone.history.start()
-//     }
-// })
-// var router = new GithubRouter()
-
-// var node = document.querySelector('form')
-
-// node.addEventListener('submit', (e) => {
-//     e.preventDefault()
-//  window.location.hash = node.querySelector('input').value
-// })
